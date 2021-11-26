a0f1ce6d939297f03587504fab9fd23be9e3973a
jonathanng11 <jonathan.nandika19@gmail.com>
2021-11-26 15:56:19 +0700

    crud category

---
+++ resources/views/admin/category/create.blade.php
@@ -0,0 +1,61 @@
+@extends('admin.layouts.main')
+
+@section('container')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Add New Category</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="admin/products"> Back</a>
+        </div>
+    </div>
+</div>
+   
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+   
+<form action="/admin/category" method="POST" enctype="multipart/form-data">
+    @csrf
+  
+    <div class="mb-3">
+    <label for="nama_category" class="form-label">Nama Kategori</label>
+    <input type="text" class="form-control @error('nama_category') is-invalid @enderror" id="nama_category" name="nama_category">
+    @error('nama_category')
+    <div class="invalid-feedback">
+        {{ $message }}
+    </div>
+    @enderror
+  </div>
+
+    <div class="mb-3">
+        <label for="kode_category" class="form-label ">Kode Kategori</label>
+        <input type="text" class="form-control @error('kode_category') is-invalid @enderror" id="kode_category" name="kode_category">
+        @error('kode_category')
+        <div class="invalid-feedback">
+            {{ $message }}
+        </div>
+        @enderror
+    </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Foto Kategori :</strong>
+                <input type="file" name="foto_category" class="form-control" placeholder="Image">
+            </div>
+        </div>
+        
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+@endsection
---
+++ resources/views/admin/category/index.blade.php
@@ -0,0 +1,47 @@
+@extends('admin.layouts.main')
+
+@section('container')
+<div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+    <h1 class="h2">Table Product </h1>      
+</div>
+
+@if(session()->has('success'))
+<div class="alert alert-success col-lg-8" role="alert">
+  {{ session('success') }}
+</div>
+@endif
+
+<div class="table-responsive">
+    <a href="/admin/products/create" class="btn btn-primary mb-3">Create New Category</a>
+        <table class="table table-striped table-sm">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Image</th>
+              <th scope="col">Category Name</th>
+              <th scope="col">Category Code</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+          @foreach ($produk as $p )
+          <tr>
+              <td>{{ $loop->iteration }}</td>
+              <td> <img style="width: 30px"src="{{ asset('storage/'.$p->foto_category) }}" class="img-fluid mt-3 mb-3"></td>
+              <td>{{ $p->nama_category }}</td>
+              <td>{{ $p->kode_category}}</td>
+              
+              <td>
+               <a href="/admin/products/{{ $p->id }}/edit" class="badge bg-warning"><span data-feather="edit"></span></a>
+                <form action="{{ url('admin/products', $p->id) }}" method="post" class="d-inline">
+                @csrf
+                @method('delete')
+                <button class="badge bg-danger border-0" onclick="return confirm('yakin ingin hapus?')"><span data-feather="x-circle"></span></button>
+                </form>
+              </td>
+          </tr>
+          @endforeach
+          </tbody>
+</div>
+
+@endsection
